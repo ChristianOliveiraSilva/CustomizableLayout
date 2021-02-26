eebb60bbba7e4c3b75b7a920cb151ffde96618e3
ChristianOliveiraSilva <gamesparamundo123@gmail.com>
2021-02-25 22:17:34 -0400

    fix bug

--- src/Pages/Admin/Components/Header.js
+++ src/Pages/Admin/Components/Header.js
@@ -4,9 +4,7 @@ class Base extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            
-        }
+        this.state = {}
     }
 
     render() {
